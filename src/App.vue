d9e261ca9976a2c63a524bb49e1a4406bb507ab6
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-30 22:34:49 +0200

    [UPD] Add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 </script>
 
 <template>
   <AppHeader />
   <AppMain />
+  <AppFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-50">
+          <div class="info">
+            <h3>Boolando s.r.l.</h3>
+            <a href="">Informazioni legali</a>
+            <a href="">Informativa sulla privacy</a>
+            <a href="">Diritto di recesso</a>
+          </div>
+        </div>
+        <div class="col-50">
+          <div class="social">
+            <h4>Trovaci anche su</h4>
+            <div class="social-logo">
+              <img src="./img/twitter-square-icon.webp" alt="twitter" />
+              <img src="./img/facebook-square-icon.png" alt="facebook" />
+              <img src="./img/Black_Instagram_icon.svg.png" alt="instagram" />
+              <img src="./img/pinterest-square-512.webp" alt="pinterest" />
+              <img src="./img/youtube-square.png" alt="youtube" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/main.scss";
+</style>
