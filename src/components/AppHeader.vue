6f98e2682f66b2831a54f6d80ec67dd29c1ab6ce
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-30 18:55:09 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-33">
+          <div class="categories">
+            <a href="">Donna</a>
+            <a href="">Uomo</a>
+            <a href="">Bambini</a>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <a href="">
+            <img class="logo" src="/img/boolean-logo.png" alt="logo" />
+          </a>
+        </div>
+        <div class="col-33">
+          <div class="container-icon">
+            <img class="icon" src="/img/user.png" alt="user" />
+            <img class="icon" src="/img/like.png" alt="like" />
+            <img class="icon" src="/img/shopping-bag.png" alt="shop" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "./src/assets/styles/main.scss";
+</style>
